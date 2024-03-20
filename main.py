0484a6a6c1e423eb36970d8517aa17da46c57a67
Rudys Natanael Acosta Crousset <ncrousset926@gmail.com>
2024-03-20 14:34:13 -0400

    start proejct api

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/products")
+def read_item():
+    with open('products.json', 'r') as file:
+        products =  json.load(file)
+    return products
