1a6e834159623aa728dd68b58c43524e2ecf002b
Rudys Natanael Acosta Crousset <ncrousset926@gmail.com>
2024-03-22 03:04:10 -0400

    Create de model

---
+++ app/models/category.py
@@ -0,0 +1,16 @@
+from sqlalchemy.schema import CreateTable
+from sqlalchemy import Column, Integer, String,TIMESTAMP
+from sqlalchemy.sql import func
+from sqlalchemy.dialects import mysql
+from config.database import Base
+
+
+class Category(Base):
+
+    __tablename__ = 'categories'
+    
+    id  =  Column(Integer, primary_key = True)
+    name = Column(String(100), nullable= False)
+    image = Column(String(100))
+    createdAt = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    updatedAt = Column(TIMESTAMP(timezone=True), onupdate=func.now())
